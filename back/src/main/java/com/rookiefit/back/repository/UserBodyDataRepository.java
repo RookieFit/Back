a9e7f2eb3836f7229a18b3488ab92035a2c45223
mjKim <106668821+pipakmj@users.noreply.github.com>
2024-11-12 13:32:10 +0900

    Feat]#28-유저체성분날짜유일성업데이트및프로필아이디유일성업데이트

--- back/src/main/java/com/rookiefit/back/repository/UserBodyDataRepository.java
+++ back/src/main/java/com/rookiefit/back/repository/UserBodyDataRepository.java
@@ -7,10 +7,18 @@ import org.springframework.stereotype.Repository;
 
 import com.rookiefit.back.entity.UserBodyDataEntity;
 
+import jakarta.transaction.Transactional;
+
 @Repository
 public interface UserBodyDataRepository extends JpaRepository<UserBodyDataEntity ,Long> {
     
     boolean existsByUserId(String userId);
 
+    boolean existsByDate(String date);
+
     List<UserBodyDataEntity> findByUserId(String userId);
+
+    @Transactional
+    void deleteAllByDate(String date);
+
 }
--- back/src/main/java/com/rookiefit/back/service/implement/UserDataServiceImplement.java
+++ back/src/main/java/com/rookiefit/back/service/implement/UserDataServiceImplement.java
@@ -38,6 +38,10 @@ public class UserDataServiceImplement implements UserDataService{
     public ResponseEntity<? super InputUserProfileResponseDto> inputUserProfile(InputUserProfileRequestDto dto) {
         try {
             String currentUserId = jwtProvider.getUserIdFromToken(dto.getToken());
+            boolean isExsitedId = userProfileRepository.existsByUserId(currentUserId);
+            if(isExsitedId){
+                userProfileRepository.deleteAllByUserId(currentUserId);
+            }
             dto.setToken(currentUserId);
 
             UserProfileEntity userProfileEntity = new UserProfileEntity(dto);
@@ -68,6 +72,11 @@ public class UserDataServiceImplement implements UserDataService{
     public ResponseEntity<? super InputUserBodyDataResponseDto> inputUserBodyData(InputUserBodyDataRequestDto dto) {
         try {
             String currentId = jwtProvider.getUserIdFromToken(dto.getToken());
+            String currentDate = CurrentDate.currentDateString();
+            boolean isExsitedId = userBodyDataRepository.existsByDate(currentDate);
+            if(isExsitedId){
+                userBodyDataRepository.deleteAllByDate(currentDate);
+            }
             dto.setToken(currentId);
             
             UserBodyDataEntity userBodyDataEntity = new UserBodyDataEntity(dto);
