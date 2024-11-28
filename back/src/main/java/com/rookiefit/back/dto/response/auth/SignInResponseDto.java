7c836ef0a78926713dfa100276db8d29d39db5b7
21270003 <rudddms1533@naver.com>
2024-11-28 18:14:44 +0900

    [feat]36수정본

--- back/src/main/java/com/rookiefit/back/dto/response/auth/SignInResponseDto.java
+++ back/src/main/java/com/rookiefit/back/dto/response/auth/SignInResponseDto.java
@@ -18,8 +18,7 @@ public class SignInResponseDto extends ResponseDto {
     private SignInResponseDto(String token) {
         super();
         this.token = token;
-        // 나중에 3600으로 수정하깅!
-        this.expirationTime = 3600000;
+        this.expirationTime = 3600;
     }
 
     public static ResponseEntity<SignInResponseDto> success(String token) {
