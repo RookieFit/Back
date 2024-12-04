006506fd0e7a925e5be348caa0a819702a6afc44
21270003 <rudddms1533@naver.com>
2024-12-04 10:33:27 +0900

    ㅗㅗ

--- back/scripts/start-server.sh
+++ back/scripts/start-server.sh
@@ -1,8 +1,6 @@
-#!/bin/bash
-
 echo "--------------- 서버 배포 시작 -----------------"
-cd /home/ubuntu/instagram-server
-npm i
-pm2 kill
-pm2 start dist/main.js --name "backend-server"
+docker stop nodejs-git-github || true
+docker rm nodejs-git-github || true
+docker pull 668701699473.dkr.ecr.ap-northeast-2.amazonaws.com/pipa-nodejs-git-github-repository:latest
+docker run -d --name nodejs-git-github -p 3000:3000 668701699473.dkr.ecr.ap-northeast-2.amazonaws.com/pipa-nodejs-git-github-repository:latest
 echo "--------------- 서버 배포 끝 -----------------"
