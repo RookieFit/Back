54d6635c9d060c9e5c125fbd61a5b8655ae50fe8
21270003 <rudddms1533@naver.com>
2024-12-04 19:05:01 +0900

    test15

--- back/scripts/start-new-server.sh
+++ back/scripts/start-new-server.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
+set -e
+
 # Docker 서비스 상태 확인
 if ! systemctl is-active --quiet docker; then
   echo "Docker service is not running. Starting Docker..."
   sudo systemctl start docker
 fi
 
-# 기존 컨테이너가 실행 중인지 확인
-if [ $(docker ps -q -f name=rookiefit-server) ]; then
-  echo "Stopping existing container..."
+# Green 환경 실행
+echo "Starting new container (Green)..."
+docker pull 739275460428.dkr.ecr.ap-northeast-2.amazonaws.com/rookiefit-server:latest
+docker run -d --name rookiefit-server-green -p 4041:4040 739275460428.dkr.ecr.ap-northeast-2.amazonaws.com/rookiefit-server:latest
+
+# 기존 Blue 컨테이너 종료
+if docker ps -q -f name=rookiefit-server; then
+  echo "Stopping and removing old container (Blue)..."
   docker stop rookiefit-server
   docker rm rookiefit-server
 fi
 
-# 새로운 컨테이너 실행
-docker run -d --name rookiefit-server-green -p 4041:4040 ${{ steps.login-ecr.outputs.registry }}/rookiefit-server:latest
-
-# 기존 컨테이너를 멈추지 않고 유지
-docker stop rookiefit-server || true
-docker rm rookiefit-server || true
-
-# Green 환경을 Blue로 전환
+# Green을 Blue로 전환
+echo "Switching Green to Blue..."
 docker rename rookiefit-server-green rookiefit-server
 
-# 기존 컨테이너를 재시작하여 최신 버전 반영
-docker restart rookiefit-server
+# 최종 확인
+echo "Deployment completed successfully."
+docker ps
