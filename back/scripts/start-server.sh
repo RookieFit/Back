7d5e450dbe0a1ad31ea5e2e8375c3f229d0a1d52
21270003 <rudddms1533@naver.com>
2024-12-04 11:13:50 +0900

    ㅇㄹㄴ

--- back/scripts/start-server.sh
+++ back/scripts/start-server.sh
@@ -1,6 +1,6 @@
 echo "--------------- 서버 배포 시작 -----------------"
-docker stop nodejs-git-github || true
-docker rm nodejs-git-github || true
-docker pull 668701699473.dkr.ecr.ap-northeast-2.amazonaws.com/pipa-nodejs-git-github-repository:latest
-docker run -d --name nodejs-git-github -p 3000:3000 668701699473.dkr.ecr.ap-northeast-2.amazonaws.com/pipa-nodejs-git-github-repository:latest
+docker stop RookieFit || true
+docker rm RookieFit || true
+docker pull 668701699473.dkr.ecr.ap-northeast-2.amazonaws.com/rookiefit-server:latest
+docker run -d --name RookieFit -p 3000:3000 668701699473.dkr.ecr.ap-northeast-2.amazonaws.com/rookiefit-server:latest
 echo "--------------- 서버 배포 끝 -----------------"
