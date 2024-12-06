046e0dc73c632e7d518929bf44eab3cbf07c3835
mjKim <106668821+pipakmj@users.noreply.github.com>
2024-12-06 18:35:46 +0900

    [Feat]trainer수정-#64

--- back/src/main/java/com/rookiefit/back/dto/response/trainer/InputTrainerResponseDto.java
+++ back/src/main/java/com/rookiefit/back/dto/response/trainer/InputTrainerResponseDto.java
@@ -10,30 +10,15 @@ public class InputTrainerResponseDto {
 
     private boolean isLicensed; // 라이센스 승인 여부
     private String message;
-
-    public static ResponseEntity<InputTrainerResponseDto> success(String message) {
-        InputTrainerResponseDto responseBody = new InputTrainerResponseDto(message);
-        return ResponseEntity.status(HttpStatus.OK).body(responseBody);
-    }
-
+    
     public InputTrainerResponseDto() {
         super();
     }
 
     public InputTrainerResponseDto(String message) {
         this.message = message;
-
-    private String message;
-
-    // 기본 생성자
-    public InputTrainerResponseDto() {
-        super();
     }
 
-    // 메시지를 받는 생성자
-    public InputTrainerResponseDto(String message) {
-        this.message = message;
-    }
 
     // 성공 응답을 반환하는 static 메소드
     public static ResponseEntity<InputTrainerResponseDto> success(String message) {
